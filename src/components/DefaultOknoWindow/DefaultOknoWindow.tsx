be5572c8b2d766ca1cf44dcbff992f728fbaf305
Erik Jenkins <ejenkins132@gmail.com>
2021-11-30 19:18:06 -0500

    Styled default window

--- src/components/DefaultOknoWindow/DefaultOknoWindow.tsx
+++ src/components/DefaultOknoWindow/DefaultOknoWindow.tsx
@@ -1,3 +1,5 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import {
   OknoClose,
   OknoContent,
@@ -15,20 +17,52 @@ interface DefaultOknoWindowProps {
   children?: React.ReactNode;
 }
 
+const TitlebarStyles = css`
+  display: flex;
+  justify-content: space-between;
+  background: #888;
+  padding: 0.25rem;
+
+  & > * {
+    margin-left: 2px;
+  }
+`;
+
+const ContentStyles = css`
+  background: #ddd;
+  color: #222;
+  padding: 0.5rem 0.25rem;
+`;
+
+const FooterStyles = css`
+  background: #ddd;
+  display: flex;
+  justify-content: end;
+`;
+
+const ResizeStyles = css`
+  width: 12px;
+  height: 12px;
+  background: #aaa;
+`;
+
 export default function DefaultOknoWindow({
   okno,
   children,
 }: DefaultOknoWindowProps) {
   return (
     <OknoWindow okno={okno}>
-      <OknoTitlebar>
-        <OknoMinimize>Minimize</OknoMinimize>
-        <OknoMaximize>Maximize</OknoMaximize>
-        <OknoClose>Close</OknoClose>
+      <OknoTitlebar css={TitlebarStyles}>
+        <span>Window {okno.id}</span>
+        <div className="controls">
+          <OknoMinimize>Minimize</OknoMinimize>
+          <OknoMaximize>Maximize</OknoMaximize>
+          <OknoClose>Close</OknoClose>
+        </div>
       </OknoTitlebar>
-      <OknoContent>{children}</OknoContent>
-      <OknoFooter>
-        <OknoResize />
+      <OknoContent css={ContentStyles}>{children}</OknoContent>
+      <OknoFooter css={FooterStyles}>
+        <OknoResize css={ResizeStyles} />
       </OknoFooter>
     </OknoWindow>
   );
